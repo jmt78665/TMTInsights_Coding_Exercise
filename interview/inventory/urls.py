88801eacaa38ce2ad4326f58f2b85d50ad52c879
Joe Trujillo <jmt78665@gmail.com>
2023-04-23 08:36:30 -0500

    Added inventory-list/<str:created_after> url for InventoryListView

--- interview/inventory/urls.py
+++ interview/inventory/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from interview.inventory.views import InventoryLanguageListCreateView, InventoryLanguageRetrieveUpdateDestroyView, InventoryListCreateView, InventoryRetrieveUpdateDestroyView, InventoryTagListCreateView, InventoryTagRetrieveUpdateDestroyView, InventoryTypeListCreateView, InventoryTypeRetrieveUpdateDestroyView
+from interview.inventory.views import InventoryLanguageListCreateView, InventoryLanguageRetrieveUpdateDestroyView, InventoryListCreateView, InventoryRetrieveUpdateDestroyView, InventoryTagListCreateView, InventoryTagRetrieveUpdateDestroyView, InventoryTypeListCreateView, InventoryTypeRetrieveUpdateDestroyView, InventoryListView
 from interview.order.views import OrderListCreateView, OrderTagListCreateView
 
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('tags/', InventoryTagListCreateView.as_view(), name='inventory-tags-list'),
     path('types/', InventoryTypeListCreateView.as_view(), name='inventory-types-list'),
     path('', InventoryListCreateView.as_view(), name='inventory-list'),
+    path('inventory-list/<str:created_after>', InventoryListView.as_view(), name='inventory-list'),
 ]
